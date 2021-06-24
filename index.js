e90c0b8d80b6dbe899e51115424203d72ef683a4
bjmontillon <pangilinanbj@gmail.com>
2021-06-24 22:20:06 +0800

    thirdcommit

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ io.on('connection', (socket) => {
 app.use(cors());
 
 http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
+  console.log(`Socket.IO server running at https://ourtalk-chatapp.herokuapp.com/`);
 });
